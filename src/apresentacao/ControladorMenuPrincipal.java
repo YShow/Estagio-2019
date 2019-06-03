41988c3f8908ff07346403dfc94a0851e91918bb
yasser <yassermbb@gmail.com>
2019-06-03 16:43:15 -0300

    bugfix: erro ao executar por causa do static no metodo fexarProg. Maven: Atualização da versao do javafx. REMOÇÃO: removido classe de utilidade

--- src/apresentacao/ControladorMenuPrincipal.java
+++ src/apresentacao/ControladorMenuPrincipal.java
@@ -2,17 +2,12 @@ package apresentacao;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import utilidade.ControladorBotaoConfirma;
 
 public class ControladorMenuPrincipal {
 
-    @FXML
-    private static void fexarPrograma(ActionEvent event) {
-	var confirma = new ControladorBotaoConfirma();
-
-	 char sim_nao = confirma.btnChama();
-	if (sim_nao == 'y') {
-	    System.exit(0);
-	}
+ 
+    @FXML 
+    private void fexaProg(ActionEvent event) {
+	
     }
 }
