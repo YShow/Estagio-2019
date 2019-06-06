9dee124877c95ce4c1f3b74dc732d1cec2a41f6d
yasser <yassermbb@gmail.com>
2019-06-06 11:13:18 -0300

    Começo das classes de negocio

---
+++ src/negocio/NegCidade.java
@@ -0,0 +1,7 @@
+package negocio;
+import acessoBD.MariaDB.AcessoBD;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+public class NegCidade {
+
+}
---
+++ src/negocio/NegFuncionario.java
@@ -0,0 +1,9 @@
+package negocio;
+import acessoBD.MariaDB.AcessoBD;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+public class NegFuncionario {
+
+    
+    
+}
