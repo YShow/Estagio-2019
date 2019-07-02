a3c47f4ada6cc4c6a7e14a8f93f2b77f644dfdcf
yasser <yassermbb@gmail.com>
2019-07-02 17:59:19 -0300

    inserção

---
+++ src/apresentacao/insere/ControladorInserirFuncionario.java
@@ -0,0 +1,23 @@
+package apresentacao.insere;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+public class ControladorInserirFuncionario {
+    @FXML
+    private TextField txtNomeFuncionario;
+
+    @FXML
+    private PasswordField txtSenhaFuncionario;
+
+    @FXML
+    private CheckBox chkAdm;
+
+    @FXML
+    void btnGravarFuncionario(ActionEvent event) {
+
+    }
+
+
+}
