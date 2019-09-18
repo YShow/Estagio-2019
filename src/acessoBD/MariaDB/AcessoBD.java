0c7629d2b2f7aea9894012488dfcbfe8de90baf6
yasser <yassermbb@gmail.com>
2019-09-18 17:43:38 -0300

    Voltar campos estaticos em AcessoDB e novo banco de dados em utilidade
    Foi adicionado o campo "ativo" em algumas tabelas para melhor controle

--- src/acessoBD/MariaDB/AcessoBD.java
+++ src/acessoBD/MariaDB/AcessoBD.java
@@ -5,16 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class AcessoBD {
-    // USUARIO DO BANCO
-    
-    public Connection getConexao() throws SQLException {
-	final var USUARIO = "usuario";
-	    // SENHA DO BANCO
-	final var SENHA = "1234";
-	final var STRINGCONEXAO = "jdbc:mariadb://localhost:3306/cantagalo?rewriteBatchedStatements=true";
+	// USUARIO DO BANCO
+	private static final String USUARIO = "usuario";
+	// SENHA DO BANCO
+	private final String SENHA = "1234";
+	private final String STRINGCONEXAO = "jdbc:mariadb://localhost:3306/cantagalo?rewriteBatchedStatements=true";
 
-	
-	return DriverManager.getConnection(STRINGCONEXAO, USUARIO, SENHA);
-    }
+	public Connection getConexao() throws SQLException {
+
+		return DriverManager.getConnection(STRINGCONEXAO, USUARIO, SENHA);
+	}
 
 }
